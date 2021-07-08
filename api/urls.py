d273095a795ec43e466d33e3ff69b318125bb7d7
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-07-08 17:40:49 +0100

    creating apis

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import article_list, article_detail
+from .views import article_list, article_detail, ArticleApiView
 
 urlpatterns = [
-    path('article/', article_list, name='article_list'),
+    #path('article/', article_list, name='article_list'),
+    path('article/', ArticleApiView.as_view(), name='article_list'),
     path('detail/<int:pk>/', article_detail, name='article_detail'),
 ]
