bdbbdf7e295eda6aa6ea8427a5755fda73c025f7
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-07-09 15:01:32 +0100

    commit

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import article_list, article_detail, ArticleApiView
+from .views import article_list, article_detail, ArticleApiView, GenericApiView
 
 urlpatterns = [
     #path('article/', article_list, name='article_list'),
     path('article/', ArticleApiView.as_view(), name='article_list'),
     path('detail/<int:pk>/', article_detail, name='article_detail'),
+    path('generic/article/<int:id>/', GenericApiView.as_view(), name='generic_list_view'),
 ]
